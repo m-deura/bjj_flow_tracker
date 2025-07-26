876f5c5550bc2075521c11eeb6e272efbedb86d9
m-deura <aw.mayuki.deura@gmail.com>
2025-07-26 12:14:39 +0900

    update: add the constraints to model & migration files

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -1,4 +1,7 @@
 class Technique < ApplicationRecord
-  belongs_to :template_technique_id
-  belongs_to :user_id
+  belongs_to :template_technique
+  belongs_to :user
+
+  validates :name, :is_submission, :mastery_level, :is_bookmarked, presence: true
+  validates :name, uniquness: true
 end
--- db/migrate/20250726022713_create_techniques.rb
+++ db/migrate/20250726022713_create_techniques.rb
@@ -1,8 +1,8 @@
 class CreateTechniques < ActiveRecord::Migration[7.2]
   def change
     create_table :techniques do |t|
-      t.references :template_technique_id, foreign_key: true
-      t.references :user_id, null: false, foreign_key: true
+      t.references :template_technique, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :name, null: false
       t.text :note
       t.boolean :is_submission, null: false, default: false
@@ -10,8 +10,8 @@ class CreateTechniques < ActiveRecord::Migration[7.2]
       t.boolean :is_bookmarked, null: false, default: false
 
       t.timestamps
-
-      add_index :techniques, :name, unique: true
     end
+
+    add_index :techniques, :name, unique: true
   end
 end
