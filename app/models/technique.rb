0f8f780025301230e8d3e1df1e8e0c59d2d19dd0
m-deura <aw.mayuki.deura@gmail.com>
2025-07-27 22:20:09 +0900

    update: noteカラムをtransitionsテーブルに移行することを鑑み、techniquesテーブルの設計を修正

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -3,9 +3,9 @@ class Technique < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, uniqueness: true
-  validates :mastery_level, presence: true
+  # validates :mastery_level, presence: true
   validates :is_submission, inclusion: { in: [ true, false ] }
-  validates :is_bookmarked, inclusion: { in: [ true, false ] }
+  # validates :is_bookmarked, inclusion: { in: [ true, false ] }
 
-  enum :mastery_level, { not_learned: 0, familiar: 1, practicing: 2, almost_there: 3, perfect: 4 }
+  # enum :mastery_level, { not_learned: 0, familiar: 1, practicing: 2, almost_there: 3, perfect: 4 }
 end
--- db/migrate/20250726022713_create_techniques.rb
+++ db/migrate/20250726022713_create_techniques.rb
@@ -2,12 +2,12 @@ class CreateTechniques < ActiveRecord::Migration[7.2]
   def change
     create_table :techniques do |t|
       #      t.references :template_technique, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :user, foreign_key: true
       t.string :name, null: false
-      t.text :note
+      t.string :english_name
       t.boolean :is_submission, null: false, default: false
-      t.integer :mastery_level, null: false, default: 1
-      t.boolean :is_bookmarked, null: false, default: false
+      # t.integer :mastery_level, null: false, default: 1
+      # t.boolean :is_bookmarked, null: false, default: false
 
       t.timestamps
     end
