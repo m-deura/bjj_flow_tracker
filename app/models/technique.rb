b2ab783b86b7929c48e3ab49318a7c33e6a011d1
m-deura <aw.mayuki.deura@gmail.com>
2025-07-27 14:52:23 +0900

    fix: is_bookmarked: false が blank と判定されてしまうためモデル制約を修正

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -2,6 +2,10 @@ class Technique < ApplicationRecord
   #  belongs_to :template_technique
   belongs_to :user
 
-  validates :name, :is_submission, :mastery_level, :is_bookmarked, presence: true
-  validates :name, uniquness: true
+  validates :name, presence: true, uniqueness: true
+  validates :mastery_level, presence: true
+  validates :is_submission, inclusion: { in: [ true, false ] }
+  validates :is_bookmarked, inclusion: { in: [ true, false ] }
+
+  enum :mastery_level, { not_learned: 0, familiar: 1, practicing: 2, almost_there: 3, perfect: 4 }
 end
