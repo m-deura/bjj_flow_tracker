f448aab19c16b880cc9fef5bb295bc5250a813dc
m-deura <aw.mayuki.deura@gmail.com>
2025-08-05 08:21:31 +0900

    fix: チャートデータ取得用APIのロジックを微調整

--- app/controllers/api/v1/charts_controller.rb
+++ app/controllers/api/v1/charts_controller.rb
@@ -5,12 +5,12 @@ class Api::V1::ChartsController < ApplicationController
     nodes = current_user.charts.first.nodes
 
     nodes_data = nodes.map do |node|
-      { data: { id: "#{node.id}", label: node.technique.name } }
+      { data: { id: node.id.to_s, label: node.technique.name } }
     end
 
     edges_data = nodes.map do |node|
       if node.has_parent?
-        { data: { source: node.parent_id, target: node.id } }
+        { data: { source: node.parent_id.to_s, target: node.id.to_s } }
       end
     end.compact # has_parent?に該当しないnodeはnilを返すため、これを排除する。
 
