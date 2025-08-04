f63157d7753be17753707f92e7941719e97691dc
m-deura <aw.mayuki.deura@gmail.com>
2025-08-04 22:39:28 +0900

    update: チャート描写用のデータを返却するAPIロジックを記述

--- app/controllers/api/v1/charts_controller.rb
+++ app/controllers/api/v1/charts_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::ChartsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    nodes = current_user.charts.first.nodes
+
+    nodes_data = nodes.map do |node|
+      { data: { id: "#{node.id}", label: node.technique.name } }
+    end
+
+    edges_data = nodes.map do |node|
+      if node.has_parent?
+        { data: { source: node.parent_id, target: node.id } }
+      end
+    end.compact # has_parent?に該当しないnodeはnilを返すため、これを排除する。
+
+    render json: (nodes_data + edges_data), status: :ok
+  end
 end
