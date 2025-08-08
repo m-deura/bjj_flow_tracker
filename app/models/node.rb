b455d7511c757975f7a9aa2811d8425957daf859
m-deura <aw.mayuki.deura@gmail.com>
2025-08-08 13:54:15 +0900

    fix: 子ノード追加時のバリデーションエラーへの対処

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,6 +1,5 @@
 class Node < ApplicationRecord
   belongs_to :chart, optional: true
   belongs_to :technique, optional: true
-  belongs_to :user
   has_ancestry ancestry_format: :materialized_path2
 end
