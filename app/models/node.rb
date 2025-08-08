94ce32c7a9289ea95f90051f18472a2574634a7b
m-deura <aw.mayuki.deura@gmail.com>
2025-08-08 10:48:59 +0900

    fix: nodes_controllerのロジックが書きやすくなるよう、モデル定義見直し

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,5 +1,6 @@
 class Node < ApplicationRecord
   belongs_to :chart, optional: true
   belongs_to :technique, optional: true
+  belongs_to :user
   has_ancestry ancestry_format: :materialized_path2
 end
