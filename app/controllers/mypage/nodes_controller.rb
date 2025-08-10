07076da6731b2c1c7255a83cef199b1e369fd816
m-deura <aw.mayuki.deura@gmail.com>
2025-08-10 17:19:56 +0900

    chore: 将来的な拡張のため、nodes更新時にtransitionsを更新する処理をコメントアウトで追記

--- app/controllers/mypage/nodes_controller.rb
+++ app/controllers/mypage/nodes_controller.rb
@@ -18,6 +18,7 @@ class Mypage::NodesController < Mypage::BaseController
   def update
     @node = current_user.nodes.find(params[:id])
     chart = @node.chart
+    # technique = @node.technique
 
     selected_ids = Array(update_params[:children_ids]).compact_blank.map(&:to_i)
     current_ids = @node.children.pluck(:technique_id)
@@ -33,6 +34,17 @@ class Mypage::NodesController < Mypage::BaseController
     end
 
     @node.children.where(technique_id: ids_to_remove).destroy_all
+
+    # 子ノード追加時にtransitionを更新する
+    # ids_to_add.each do |tid|
+    #  technique.outgoing_transitions.find_or_create_by!(
+    #    user: current_user,
+    #    to_technique_id: tid
+    #  )
+    # end
+    # ids_to_remove.each do |tid|
+    #  technique.outgoing_transitions.where(to_technique_id: tid).destroy_all
+    # end
 
     redirect_to mypage_chart_path(chart), notice: "展開先テクニックを更新しました", status: :see_other
   end
