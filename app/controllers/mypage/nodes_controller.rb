0d14638b04b4d0357d561385296ae801b8e7954e
m-deura <aw.mayuki.deura@gmail.com>
2025-08-11 11:05:26 +0900

    update: 最小限のルートノード作成機能を実装

--- app/controllers/mypage/nodes_controller.rb
+++ app/controllers/mypage/nodes_controller.rb
@@ -1,9 +1,20 @@
 class Mypage::NodesController < Mypage::BaseController
   def new
-    @chart = current_user.charts.first
+    @chart = current_user.charts.find(params[:chart_id])
+    @techniques = current_user.techniques.all
+    @node = @chart.nodes.build
   end
 
   def create
+    @chart = current_user.charts.find(params[:chart_id])
+    @node = @chart.nodes.build(create_params)
+
+    if @node.save
+      redirect_to mypage_chart_path(@chart), notice: "フローの開始点を作成しました"
+    else
+      flash.now[:alert] = "保存できませんでした"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -60,6 +71,10 @@ class Mypage::NodesController < Mypage::BaseController
   end
 
   private
+
+  def create_params
+    params.require(:node).permit(:technique_id)
+  end
 
   def update_params
     # 展開先テクニックに何も選択されていない状態で更新をかけるとparams[:node]自体が送られないので、空配列も許容する。
