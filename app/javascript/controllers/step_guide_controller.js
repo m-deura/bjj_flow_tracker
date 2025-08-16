2bf24023dcff737a3b76c8bfab6a4758a79ba6dd
m-deura <aw.mayuki.deura@gmail.com>
2025-08-16 18:08:55 +0900

    update: 最低限のマイページステップガイドを実装

--- app/javascript/controllers/step_guide_controller.js
+++ app/javascript/controllers/step_guide_controller.js
@@ -4,9 +4,11 @@ import introJs from "intro.js"
 // Connects to data-controller="step-guide"
 export default class extends Controller {
   connect() {
+		// introJs.tour().setOption("dontShowAgain", true).start();
   }
 
 	startMenuGuide() {
-		console.log("MenuGuide started!")
+		// console.log("MenuGuide started!")
+		introJs.tour().start();
 	}
 }
