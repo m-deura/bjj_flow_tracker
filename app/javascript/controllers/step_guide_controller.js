2da18177574bf80fcf345dc421a26bea23715c5d
m-deura <aw.mayuki.deura@gmail.com>
2025-08-16 16:43:32 +0900

    update: step-guideコントローラーの接続を確認

--- app/javascript/controllers/step_guide_controller.js
+++ app/javascript/controllers/step_guide_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
+import introJs from "intro.js"
 
 // Connects to data-controller="step-guide"
 export default class extends Controller {
   connect() {
   }
+
+	startMenuGuide() {
+		console.log("MenuGuide started!")
+	}
 }
