890de1310726e4b3892f3cecf939148785f3c627
m-deura <aw.mayuki.deura@gmail.com>
2025-08-18 07:42:04 +0900

    add: テクニック一覧画面のステップガイドを実装

--- app/javascript/controllers/step_guide_controller.js
+++ app/javascript/controllers/step_guide_controller.js
@@ -43,12 +43,13 @@ export default class extends Controller {
 	}
 
 	startTechniqueGuide() {
-		console.log("Hello!")
 		const step0 = document.querySelector("#step0")
 		const step1 = document.querySelector("#step1")
 		const step2 = document.querySelector("#step2")
+		const step3 = document.querySelector("#step3")
+		const step4 = document.querySelector("#step4")
 
-		introJs.tour().setOptions({
+		const intro = introJs.tour().setOptions({
 			steps: [
 				{
 					intro: step0.dataset.introText
@@ -63,17 +64,60 @@ export default class extends Controller {
 					title: step2.dataset.titleText,
 					intro: step2.dataset.introText
 				},
+				{
+					title: step3.dataset.titleText,
+					intro: step3.dataset.introText
+				},
+				{
+					element: document.querySelector(".collapse"),
+					title: step4.dataset.titleText,
+					intro: step4.dataset.introText
+				},
 			]
-		}).start();
+		})
+
+	intro.onAfterChange((el) => {
+  	// console.log("今ハイライトしてるのは:", el)
+		// console.log(intro.getCurrentStep())
+
+		/* テクニック詳細画面ガイドの直前にカードを開こうと努めた際の名残。
+		function waitUntilVisible(card, { timeout = 500, interval = 50 } = {}){
+				return new Promise((resolve) => {
+					const start = performance.now();
+
+					(function check(){
+						const content = card.querySelector(".collapse-content");
+						if (content && content.offsetHeight > 0){
+							return resolve();	
+						}
+						if (performance.now() - start > timeout){
+							return resolve();
+						}
+						setTimeout(check, interval);
+					})();
+				});
+			}
+			*/
+
+		// 一番上にあるカードを展開し、カード内部(テクニック詳細画面)の説明をする。
+		if (intro.getCurrentStep() === 3) {
+			const card = document.querySelector(".collapse");
+			if (card) card.querySelector("input[type='checkbox']").checked = true;
+
+			// await waitUntilVisible(card);
+			}
+	})
+
+		intro.start();
+		// console.log(intro)
 	}
 
-
 	startChartGuide() {
-		const step0 = this.step0Target
-		const step1 = this.step1Target
-		const step2 = this.step2Target
-		const step3 = this.step3Target
-		const step4 = this.step4Target
+		const step0 = document.querySelector("#step0")
+		const step1 = document.querySelector("#step1")
+		const step2 = document.querySelector("#step2")
+		const step3 = document.querySelector("#step3")
+		const step4 = document.querySelector("#step4")
 
 		introJs.tour().setOptions({
 			steps: [
