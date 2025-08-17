85f19dfa1f3f4608c8b46345e0d348f7095f8561
m-deura <aw.mayuki.deura@gmail.com>
2025-08-17 10:53:34 +0900

    fix: テクニックのカテゴリー定義をseeds.rbに合わせて修正

--- app/models/technique_preset.rb
+++ app/models/technique_preset.rb
@@ -4,5 +4,5 @@ class TechniquePreset < ApplicationRecord
   validates :name_ja, presence: true, uniqueness: true
   validates :name_en, presence: true, uniqueness: true
 
-  enum :category, { submission: 0, sweep: 1, pass: 2, guard: 3, back_take: 4, pin: 5, takedown: 6, control: 7 }
+  enum :category, { submission: 0, sweep: 1, pass: 2, guard: 3, control: 4, takedown: 5 }
 end
