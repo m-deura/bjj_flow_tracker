41d4f7f6815cb01b23bd690eb258f28d79d60e79
m-deura <aw.mayuki.deura@gmail.com>
2025-08-17 10:54:26 +0900

    update: テクニック一覧ページにステップガイド開始用のツールチップを設置

--- app/javascript/controllers/step_guide_controller.js
+++ app/javascript/controllers/step_guide_controller.js
@@ -42,4 +42,76 @@ export default class extends Controller {
 			]
 		}).start();
 	}
+
+	startTechniqueGuide() {
+		console.log("Hello!")
+		const step0 = this.step0Target
+		const step1 = this.step1Target
+		const step2 = this.step2Target
+		const step3 = this.step3Target
+		const step4 = this.step4Target
+
+		introJs.tour().setOptions({
+			steps: [
+				{
+					title: step0.dataset.titleText,
+					intro: step0.dataset.introText
+				},
+				{
+					element: step1,
+					title: step1.dataset.titleText,
+					intro: step1.dataset.introText
+				},
+				{
+					element: step2,
+					title: step2.dataset.titleText,
+					intro: step2.dataset.introText
+				},
+				{
+					element: step3,
+					title: step3.dataset.titleText,
+					intro: step3.dataset.introText
+				},
+				{  
+					intro: step4.dataset.introText
+				},
+			]
+		}).start();
+	}
+
+
+	startChartGuide() {
+		const step0 = this.step0Target
+		const step1 = this.step1Target
+		const step2 = this.step2Target
+		const step3 = this.step3Target
+		const step4 = this.step4Target
+
+		introJs.tour().setOptions({
+			steps: [
+				{
+					title: step0.dataset.titleText,
+					intro: step0.dataset.introText
+				},
+				{
+					element: step1,
+					title: step1.dataset.titleText,
+					intro: step1.dataset.introText
+				},
+				{
+					element: step2,
+					title: step2.dataset.titleText,
+					intro: step2.dataset.introText
+				},
+				{
+					element: step3,
+					title: step3.dataset.titleText,
+					intro: step3.dataset.introText
+				},
+				{  
+					intro: step4.dataset.introText
+				},
+			]
+		}).start();
+	}
 }
