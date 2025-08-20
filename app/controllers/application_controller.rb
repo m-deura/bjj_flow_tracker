3300e81613c42ec70b3061ddc54808f7f50389cf
m-deura <aw.mayuki.deura@gmail.com>
2025-08-20 09:24:16 +0900

    update: トップ画面のデザインを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :authenticate_user!
+  before_action :authenticate_user!, if: :user_signed_in?
+  before_action :set_chart
+
+  private
+
+  # ヘッダーにある Flow Chartリンクのために @chart を取得。
+  # TODO: 将来的に、Flow Chartリンク先がチャート一覧になったらこの処理は削除して良い。
+  # user_signed_in? はインスタンスメソッドとしてしか利用できない(クラス直下で使用できなかった)。
+  def set_chart
+    @chart = current_user.charts.first
+  end
 
   def after_sign_in_path_for(resource)
     mypage_root_path
