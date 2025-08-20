6d8190bd0b4e8fc1625e746907087e367f8c6211
m-deura <aw.mayuki.deura@gmail.com>
2025-08-21 07:41:27 +0900

    fix: ログイン前に要ログインページにアクセスすると発生するエラーを解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :authenticate_user!, if: :user_signed_in?
-  before_action :set_chart
+  before_action :authenticate_user!
+  before_action :set_chart, if: :user_signed_in?
 
   private
 
