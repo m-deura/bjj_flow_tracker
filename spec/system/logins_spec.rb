e7a40a773e947f5864d39cb7e4b45467d32234a1
m-deura <aw.mayuki.deura@gmail.com>
2025-08-30 10:02:25 +0900

    update: 未ログイン時にマイページにアクセスできないRSpecテスト追加

--- spec/system/logins_spec.rb
+++ spec/system/logins_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe "Logins", type: :system do
     driven_by(:rack_test)
   end
 
+<<<<<<< Updated upstream
   describe "テスト" do
+=======
+  describe "ログイン・ログアウト" do
+>>>>>>> Stashed changes
     it "ログインできる" do
       omniauth_login
     end
@@ -19,7 +23,22 @@ RSpec.describe "Logins", type: :system do
       expect(page).to have_current_path(root_path)
       expect(page).to have_content("ログアウトしました。")
     end
+<<<<<<< Updated upstream
   end
 
   pending "add some scenarios (or delete) #{__FILE__}"
+=======
+
+    context "未ログインの場合" do
+      it "マイページにアクセスできない" do
+        visit mypage_root_path
+
+        expect(page).to have_current_path(root_path)
+        expect(page).to have_content("ログインしてください。")
+      end
+    end
+  end
+
+  # pending "add some scenarios (or delete) #{__FILE__}"
+>>>>>>> Stashed changes
 end
