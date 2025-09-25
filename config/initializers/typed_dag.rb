6f20ade150f00aa4a08f1941af86932f1ad3e2b6
m-deura <aw.mayuki.deura@gmail.com>
2025-09-25 14:51:37 +0900

    update: typed_dagに生やすメソッド名をancestryと区別しやすい名前に変更

--- config/initializers/typed_dag.rb
+++ config/initializers/typed_dag.rb
@@ -2,6 +2,6 @@ TypedDag::Configuration.set edge_class_name: "Edge",
                             node_class_name: "Node",
                             types: { flow: { from: :dag_parents,
                                              to: :dag_children,
-                                             all_from: :ancestors,
-                                             all_to: :descendants }
+                                             all_from: :dag_ancestors,
+                                             all_to: :dag_descendants }
                             }
