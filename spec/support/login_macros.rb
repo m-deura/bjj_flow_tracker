c2ca11100334a4862a4a09d8bb7dab2a0a937496
m-deura <aw.mayuki.deura@gmail.com>
2025-09-26 16:42:00 +0900

    update: ユーザー初ログイン時にプリセットコピーされることを確認する記述を追加

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -4,7 +4,18 @@ module LoginMacros
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
 
     visit root_path
-    click_on "Google でログイン", match: :first
+
+    tp_count = TechniquePreset.count
+    np_count = NodePreset.count
+
+    expect {
+      click_on "Google でログイン", match: :first
+    }.to change(User, :count).by(1)
     expect(page).to have_current_path(mypage_root_path(locale: I18n.locale))
+
+    new_user = User.find_by!(email: "tester1@example.com")
+    expect(new_user.techniques.count).to eq(tp_count)
+    expect(new_user.nodes.count).to eq(np_count)
+    expect(new_user.charts.count).to eq(1)
   end
 end
