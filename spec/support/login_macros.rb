9b7bf8dcc3fa4d1aab931b0ee0c74875c63ef4dd
m-deura <aw.mayuki.deura@gmail.com>
2025-09-26 16:54:59 +0900

    improve: ログインマクロのリファクタリング＋コメント追加

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -5,13 +5,17 @@ module LoginMacros
 
     visit root_path
 
-    tp_count = TechniquePreset.count
-    np_count = NodePreset.count
-
+    # ログイン後、ユーザー数が1増加することを確認
     expect {
       click_on "Google でログイン", match: :first
     }.to change(User, :count).by(1)
+
+    # ログイン後、ダッシュボード画面に遷移することを確認
     expect(page).to have_current_path(mypage_root_path(locale: I18n.locale))
+
+    # presetsがユーザー所有のテクニック・ノード・チャートにコピーされていることを確認
+    tp_count = TechniquePreset.count
+    np_count = NodePreset.count
 
     new_user = User.find_by!(email: "tester1@example.com")
     expect(new_user.techniques.count).to eq(tp_count)
