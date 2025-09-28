60640a326c86234d56642c1830ffe6ccbe0d0779
m-deura <aw.mayuki.deura@gmail.com>
2025-09-28 12:17:35 +0900

    add: ノードのeditアクションに関するRSpecテストを追加

--- spec/system/nodes_spec.rb
+++ spec/system/nodes_spec.rb
@@ -2,8 +2,64 @@ require 'rails_helper'
 
 RSpec.describe "Nodes", type: :system do
   before do
-    driven_by(:rack_test)
+    omniauth_login
+    user = User.find_by(email: "tester1@example.com")
+    @chart = user.charts.first
+    @technique = user.techniques.create! do |t|
+      t.set_name_for("test1")
+      t.note = "test note!1"
+    end
+    @node = @chart.nodes.create!(technique: @technique)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "indexアクション" do
+  end
+
+  describe "newアクション" do
+  end
+
+  describe "editアクション" do
+    it "ノードをクリックすると、ドロワーが開かれて編集フォームが表示される", :js do
+      visit mypage_chart_path(id: @chart.id, locale: I18n.locale)
+
+      # クリック発火（node.id はテストデータに合わせる）
+      # カスタムイベント生成＋発火を同時実行
+      page.execute_script(<<~JS, @node.id)
+        window.dispatchEvent(new CustomEvent('test:click-node', { detail: { id: arguments[0] } }));
+      JS
+
+      within('#node-drawer') do
+        expect(page).to have_field(I18n.t("helpers.label.technique_name"), with: @technique.name_for)
+        expect(page).to have_field(I18n.t("helpers.label.note"), with: @technique.note)
+        expect(page).to have_select(I18n.t("helpers.label.category"), selected: I18n.t("enums.category.nil"))
+      end
+    end
+
+    it "背景をクリックすると、ドロワーが閉じる", :js do
+      visit mypage_chart_path(id: @chart.id, locale: I18n.locale)
+
+      # ノードをクリックしてドロワーを開く
+      page.execute_script(<<~JS, @node.id)
+        window.dispatchEvent(new CustomEvent('test:click-node', { detail: { id: arguments[0] } }));
+      JS
+
+      # 背景をクリックする
+      find(".drawer-overlay").click
+
+      within('#node-drawer') do
+        expect(page).not_to have_field(I18n.t("helpers.label.technique_name"), with: @technique.name_for)
+        expect(page).not_to have_field(I18n.t("helpers.label.note"), with: @technique.note)
+        expect(page).not_to have_select(I18n.t("helpers.label.category"), selected: I18n.t("enums.category.nil"))
+      end
+    end
+  end
+
+  describe "createアクション" do
+  end
+
+  describe "updateアクション" do
+  end
+
+  describe "destroyアクション" do
+  end
 end
