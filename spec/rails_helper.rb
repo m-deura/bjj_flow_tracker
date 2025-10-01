a52f1412cc9ac233872b58ec61fb036f908a38bd
m-deura <aw.mayuki.deura@gmail.com>
2025-10-01 13:08:24 +0900

    update: FactoryBot読み込み設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,6 +73,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include FactoryBot::Syntax::Methods
   config.include LoginMacros
   config.include GraphMacros
   config.include LocaleMacros
