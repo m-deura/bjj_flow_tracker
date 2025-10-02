3fb185534ef001204be0c35546ea0bb131b8e851
m-deura <aw.mayuki.deura@gmail.com>
2025-10-02 11:00:03 +0900

    fix: traitブロックの記述位置を修正

--- spec/factories/techniques.rb
+++ spec/factories/techniques.rb
@@ -1,14 +1,15 @@
 FactoryBot.define do
   factory :technique do
     association :user
-    association :technique_preset, strategy: :build
+    association :technique_preset
 
     # Technique の必須項目を TechniquePreset に同期
     sequence(:name_ja) { |n| technique_preset&.name_ja || "技#{n}" }
     sequence(:name_en) { |n| technique_preset&.name_en || "Technique#{n}" }
     category { technique_preset&.category || :submission }
-  end
-  trait :no_preset do
-    technique_preset { nil }
+
+    trait :no_preset do
+      technique_preset { nil }
+    end
   end
 end
