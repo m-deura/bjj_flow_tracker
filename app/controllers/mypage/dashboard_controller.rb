3de537fc1fafd08c33c115db1711fc3474f17831
m-deura <aw.mayuki.deura@gmail.com>
2025-10-04 14:03:41 +0900

    update: ユーザー所有の最初のチャート画面へのリンクを、チャート一覧画面へのリンクに入れ替え

--- app/controllers/mypage/dashboard_controller.rb
+++ app/controllers/mypage/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class Mypage::DashboardController < ApplicationController
   def show
-    @chart = current_user.charts.first
     @technique_count = current_user.techniques.count
     @node_count = @chart ? @chart.nodes.count : 0
   end
