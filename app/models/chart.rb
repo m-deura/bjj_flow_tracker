bcafb98132ca855e860aad147533cb2e81997ffa
m-deura <aw.mayuki.deura@gmail.com>
2025-10-04 13:50:50 +0900

    update: チャート一覧画面の検索を機能させるためのransack設定を追加

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -4,4 +4,8 @@ class Chart < ApplicationRecord
   has_many :nodes, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "name" ]
+  end
 end
