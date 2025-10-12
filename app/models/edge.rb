f9ad91e4864c80214e7b657fc4e89146d56c3dd0
m-deura <aw.mayuki.deura@gmail.com>
2025-10-12 16:04:52 +0900

    update: Edgeインスタンスからchart関連のメソッドが使えるようにした

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -5,4 +5,9 @@ class Edge < ApplicationRecord
   # has_one :chart, through: :from, source: :chart
 
   validates :flow, presence: true
+
+  # typed_dagがedgeを自動生成するため、edgesテーブルに chart_id を設けることができない。
+  # その代わり、delegate で from(Nodeインスタンス)しか持っていない関連chartメソッドを使えるようにする
+  # = Edge.first.chart と Edge.first.chart_id が使えるようになる
+  delegate :chart, :chart_id, to: :from
 end
