d1e8a50c3c81beeb17b574c3cf46d6be6546ab69
m-deura <aw.mayuki.deura@gmail.com>
2025-10-12 16:01:27 +0900

    update: エッジ関連を追記

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -3,6 +3,10 @@ class Chart < ApplicationRecord
   belongs_to :chart_preset, optional: true
   has_many :nodes, dependent: :destroy
 
+  # outgoing_edges を through でたどる。
+  # 重複を避けるため、from 側だけを持ってくる（distinct は念の為）
+  has_many :edges, -> { distinct }, through: :nodes, source: :outgoing_edges
+
   validates :name, presence: true, uniqueness: { scope: :user_id }
 
   def self.ransackable_attributes(auth_object = nil)
--- app/models/node.rb
+++ app/models/node.rb
@@ -1,5 +1,7 @@
 class Node < ApplicationRecord
   belongs_to :chart
   belongs_to :technique
+  has_many :outgoing_edges, class_name: "Edge", foreign_key: :from_id, inverse_of: :from, dependent: :destroy
+  has_many :incoming_edges, class_name: "Edge", foreign_key: :to_id, inverse_of: :to, dependent: :destroy
   has_ancestry ancestry_format: :materialized_path2
 end
