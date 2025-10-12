1f3ee9909921bb10400aa4bf18f6f9cc073c43e0
m-deura <aw.mayuki.deura@gmail.com>
2025-10-12 16:04:13 +0900

    fix: ノード関連が使えない状態になっていたので修正

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,5 +1,8 @@
 class Edge < ApplicationRecord
-  belongs_to :from, class_name: "Node"
-  belongs_to :to, class_name: "Node"
+  belongs_to :from, class_name: "Node", inverse_of: :outgoing_edges
+  belongs_to :to, class_name: "Node", inverse_of: :incoming_edges
+
+  # has_one :chart, through: :from, source: :chart
+
   validates :flow, presence: true
 end
