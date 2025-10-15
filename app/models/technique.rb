e01f15602a5a4f6fbb54900dc5135b97d3db512d
m-deura <aw.mayuki.deura@gmail.com>
2025-10-15 21:52:37 +0900

    update: カテゴリー検索ができるようransack設定を拡張

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -20,7 +20,7 @@ class Technique < ApplicationRecord
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "name_ja", "name_en", "note" ]
+    [ "name_ja", "name_en", "note", "category" ]
   end
 
   LOCALE_NAME_FIELD = { ja: :name_ja, en: :name_en }.freeze
