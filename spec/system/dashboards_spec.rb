4c510f1266cfcd777fc8a56e4a06ff9b951b2d68
m-deura <aw.mayuki.deura@gmail.com>
2025-10-17 16:41:34 +0900

    fix: テスト前提条件となるログインマクロ実行処理を追加

--- spec/system/dashboards_spec.rb
+++ spec/system/dashboards_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Dashboards", type: :system do
+  before do
+    omniauth_login
+  end
+
   it "テクニック一覧画面へのリンクが機能する" do
     click_on "Technique", match: :first
     expect(page).to have_current_path(mypage_techniques_path(locale: I18n.locale))
