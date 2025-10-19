959782bfe332c5285a94875a8d0cb0d7c75bb5c3
m-deura <aw.mayuki.deura@gmail.com>
2025-10-19 14:04:03 +0900

    update: テストケース書き出し

--- spec/system/authorizations_spec.rb
+++ spec/system/authorizations_spec.rb
@@ -2,8 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "Authorizations", type: :system do
   before do
-    driven_by(:rack_test)
+    omniauth_login
+    u1 = User.find_by(email: "tester1@example.com")
+    u2 = create(:user)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "techniques" do
+    it "他ユーザーが所有するテクニックが一覧に表示されない" do
+    end
+    it "他ユーザーが所有するテクニックの編集ページを表示できない" do
+    end
+  end
+
+  describe "charts" do
+    it "他ユーザーが所有するチャートが一覧に表示されない" do
+    end
+    it "他ユーザーが所有するチャートの描写用JSONを表示できない(APIが正しく返ってこない)" do
+    end
+    it "他ユーザーが所有するチャート詳細ページを表示できない" do
+    end
+    it "他ユーザーが所有するチャート編集ページを表示できない" do
+    end
+  end
+
+  describe "nodes" do
+    it "他ユーザーが所有するチャートのノード作成ページを表示できない" do
+    end
+    it "他ユーザーが所有するノード編集ページを表示できない" do
+    end
+  end
 end
