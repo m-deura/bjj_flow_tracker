6920bac34edaa0933ce4c6508570fdc65c12e4db
m-deura <aw.mayuki.deura@gmail.com>
2025-10-25 14:19:33 +0900

    add: EdgePresetモデルに関連するRSpecモデルテストを追加

--- spec/models/edge_preset_spec.rb
+++ spec/models/edge_preset_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe EdgePreset, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    it "必須項目が揃っていれば有効" do
+      ep = build(:edge_preset)
+      expect(ep).to be_valid
+      expect(ep.errors).to be_empty
+    end
+  end
+
+  describe "リレーション" do
+    it "from が必須" do
+      ep = build(:edge_preset, from: nil)
+      expect(ep).to be_invalid
+      expect(ep.errors).to be_of_kind(:from, :blank)
+    end
+
+    it "to が必須" do
+      ep = build(:edge_preset, to: nil)
+      expect(ep).to be_invalid
+      expect(ep.errors).to be_of_kind(:to, :blank)
+    end
+  end
 end
