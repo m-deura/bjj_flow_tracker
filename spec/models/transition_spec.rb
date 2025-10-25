78b9c53e5c2e1a031104eacfd7001dd8b07015c1
m-deura <aw.mayuki.deura@gmail.com>
2025-10-25 15:11:26 +0900

    add: Transitionモデルに関連するRSpecモデルテストを追加

--- spec/models/transition_spec.rb
+++ spec/models/transition_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Transition, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    it "必須項目が揃っていれば有効" do
+      tr = build(:transition)
+      expect(tr).to be_valid
+      expect(tr.errors).to be_empty
+    end
+  end
+
+  describe "リレーション" do
+    it "from が必須" do
+      tr = build(:transition, from: nil)
+      expect(tr).to be_invalid
+      expect(tr.errors).to be_of_kind(:from, :blank)
+    end
+
+    it "to が必須" do
+      tr = build(:transition, to: nil)
+      expect(tr).to be_invalid
+      expect(tr.errors).to be_of_kind(:to, :blank)
+    end
+  end
 end
